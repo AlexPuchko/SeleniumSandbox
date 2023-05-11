b4cfdd419aedc4cb1bf8d70b5ee7d77ad1a6e596
Alex Puchko <aleshalinguini@gmail.com>
2023-05-11 16:32:09 +0300

    add test_runner.py module

---
+++ Unittest/test_runner.py
@@ -0,0 +1,6 @@
+import unittest
+import utest_calc
+calcTestSuite = unittest.TestSuite()
+calcTestSuite.addTest(unittest.makeSuite(utest_calc.CalcTest))
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(calcTestSuite)
