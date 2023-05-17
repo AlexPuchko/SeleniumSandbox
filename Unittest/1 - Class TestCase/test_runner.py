c1ed4c4de51ef9432c83556a4cf6680c3985a821
Alex Puchko <aleshalinguini@gmail.com>
2023-05-17 16:26:36 +0300

    add main classes to the Sandbox

--- Unittest/1 - Class TestCase/test_runner.py
+++ Unittest/1 - Class TestCase/test_runner.py
@@ -1,6 +1,10 @@
 import unittest
 import utest_calc
+
+
 calcTestSuite = unittest.TestSuite()
 calcTestSuite.addTest(unittest.makeSuite(utest_calc.CalcTest))
+print("count of tests: " + str(calcTestSuite.countTestCases()) + "\n")
+
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(calcTestSuite)
